f26fabe8592750467e221496bac51d647f6a88c1
vr25 <rawtevipula25@gmail.com>
2020-03-14 19:26:33 -0400

    first commit

--- dash_basic5.py
+++ dash_basic5.py
@@ -17,34 +17,85 @@ import pandas_datareader.data as web
 import numpy as np
 import copy
 
-df1 = pd.read_csv("constituents_sp_400.csv", index_col=False).head(5)
-inputStock = df1['Symbol'] #["FB", "GE", "AAPL", "GOOGL", "HPT", "A"]
-inputSector = df1['Sector']
-
 start = datetime.datetime.today() - relativedelta(days=30)
 end = datetime.datetime.today()
 
-df = pd.DataFrame()
+#small-cap
 
-for (i, j) in zip(inputStock, inputSector):
-    #print("i: ", i)
-    #print("j: ", j)
+df1_small = pd.read_csv("constituents_sp_600.csv", index_col=False).head(5)
+inputStock1 = df1_small['Symbol']
+inputSector1 = df1_small['Sector']
+
+df1 = pd.DataFrame()
+
+for (i, j) in zip(inputStock1, inputSector1):
+
     try:
-        df_ = (web.DataReader(i, 'yahoo', start=start, end=end)).reset_index()
-        df_['Symbol'] = i
-        df_['Sector'] = j
-        df = pd.concat([df, df_])
+        df1_ = (web.DataReader(i, 'yahoo', start=start, end=end)).reset_index()
+        df1_['Symbol'] = i
+        df1_['Sector'] = j
+        df1 = pd.concat([df1, df1_])
     except:
-        print("hi")
+        print("cannot fetch data from yahoo.")
 
-df['daily_returns'] = df['Adj Close'].pct_change()
-df = df.dropna()
-df['daily_cum_returns'] = (df['daily_returns'] + 1).cumprod()
-print(df)
+df1['daily_returns'] = df1['Adj Close'].pct_change()
+df1 = df1.dropna()
+df1['daily_cum_returns'] = (df1['daily_returns'] + 1).cumprod()
+
+df1_copy = df1.copy()
+df1 = df1.groupby(['Date', 'Sector']).mean().reset_index()
 
 
-df_copy = df.copy()
-df = df.groupby(['Date', 'Sector']).mean().reset_index()
+#mid-cap
+
+df1_mid = pd.read_csv("constituents_sp_400.csv", index_col=False).head(5)
+inputStock2 = df1_mid['Symbol']
+inputSector2 = df1_mid['Sector']
+
+df2 = pd.DataFrame()
+
+for (i, j) in zip(inputStock2, inputSector2):
+
+    try:
+        df2_ = (web.DataReader(i, 'yahoo', start=start, end=end)).reset_index()
+        df2_['Symbol'] = i
+        df2_['Sector'] = j
+        df2 = pd.concat([df2, df2_])
+    except:
+        print("cannot fetch data from yahoo.")
+
+df2['daily_returns'] = df2['Adj Close'].pct_change()
+df2 = df2.dropna()
+df2['daily_cum_returns'] = (df2['daily_returns'] + 1).cumprod()
+
+df2_copy = df2.copy()
+df2 = df2.groupby(['Date', 'Sector']).mean().reset_index()
+
+
+#large-cap
+
+df1_large = pd.read_csv("constituents_sp_500.csv", index_col=False).head(5)
+inputStock3 = df1_large['Symbol']
+inputSector3 = df1_large['Sector']
+
+df3 = pd.DataFrame()
+
+for (i, j) in zip(inputStock3, inputSector3):
+
+    try:
+        df3_ = (web.DataReader(i, 'yahoo', start=start, end=end)).reset_index()
+        df3_['Symbol'] = i
+        df3_['Sector'] = j
+        df3 = pd.concat([df3, df3_])
+    except:
+        print("cannot fetch data from yahoo.")
+
+df3['daily_returns'] = df3['Adj Close'].pct_change()
+df3 = df3.dropna()
+df3['daily_cum_returns'] = (df3['daily_returns'] + 1).cumprod()
+
+df3_copy = df3.copy()
+df3 = df3.groupby(['Date', 'Sector']).mean().reset_index()
 
 # Dash app
 
@@ -55,78 +106,92 @@ server = app.server
 app.config.suppress_callback_exceptions = True
 
 app.layout = html.Div([
-    dbc.Row([html.Div([
-    html.H1('Graph 1',
-    ),
+    dbc.Row([
+        dbc.col([html.Div([
 
     dcc.Dropdown(
-        id='sector-dropdown',
-        options=[{'label': i, 'value': i} for i in df.Sector.unique()],
+        id='sector1-dropdown',
+        options=[{'label': i, 'value': i} for i in df1.Sector.unique()],
         multi=True,
-        value=inputSector
-    ),
+        value=inputSector1),
 
     dcc.Graph(id='graph1')],
-    style={'width': '33.33%', 'display': 'inline-block'})
+    style={'width': '33%', 'display': 'inline-block'})
         ]),
 
-    dbc.Row([html.Div([
-    html.H1('Graph 2',
-    ),
+        dbc.col([html.Div([
 
     dcc.Dropdown(
-        id='symbol-dropdown',
-        options=[{'label': i, 'value': i} for i in df_copy.Symbol.unique()],
+        id='sector2-dropdown',
+        options=[{'label': i, 'value': i} for i in df2.Sector.unique()],
         multi=True,
-        value=[inputStock[0]]
-    ),
+        value=inputSector2),
+
+    dcc.Graph(id='graph3')],
+    style={'width': '33%', 'display': 'inline-block'})
+        ]),
+
+        dbc.col([html.Div([
+
+    dcc.Dropdown(
+        id='sector3-dropdown',
+        options=[{'label': i, 'value': i} for i in df3.Sector.unique()],
+        multi=True,
+        value=inputSector3),
+
+    dcc.Graph(id='graph5')],
+    style={'width': '33%', 'display': 'inline-block'})
+        ]),        
+        ]),
+
+
+    dbc.Row([
+        dbc.col([html.Div([
+
+    dcc.Dropdown(
+        id='symbol1-dropdown',
+        options=[{'label': i, 'value': i} for i in df1_copy.Symbol.unique()],
+        multi=True,
+        value=[inputStock1[0]]),
 
     dcc.Graph(id='graph2')],
-    style={'width': '33.33%', 'display': 'inline-block'})
-    ])
-            
+    style={'width': '33%', 'display': 'inline-block'})
+    ]),
+
+    dbc.col([html.Div([
+
+    dcc.Dropdown(
+        id='symbol2-dropdown',
+        options=[{'label': i, 'value': i} for i in df2_copy.Symbol.unique()],
+        multi=True,
+        value=[inputStock2[0]]),
+
+    dcc.Graph(id='graph4')],
+    style={'width': '33%', 'display': 'inline-block'})
+    ]),
+
+    dbc.col([html.Div([
+
+    dcc.Dropdown(
+        id='symbol3-dropdown',
+        options=[{'label': i, 'value': i} for i in df3_copy.Symbol.unique()],
+        multi=True,
+        value=[inputStock3[0]]),
+
+    dcc.Graph(id='graph6')],
+    style={'width': '33%', 'display': 'inline-block'})
+    ]),
+    ])       
 ]
 )
 
-
-'''
-app.layout = html.Div([
-html.Div([
-    html.H1('Graph 1',
-    ),
-
-    dcc.Dropdown(
-        id='sector-dropdown',
-        options=[{'label': i, 'value': i} for i in df.Sector.unique()],
-        multi=True,
-        value=inputSector
-    ),
-
-    dcc.Graph(id='graph1')],
-    style={'width': '48%', 'display': 'inline-block'}),
-
-html.Div([
-    html.H1('Graph 2',
-    ),
-
-    dcc.Dropdown(
-        id='symbol-dropdown',
-        options=[{'label': i, 'value': i} for i in df_copy.Symbol.unique()],
-        multi=True,
-        value=[inputStock[0]]
-    ),
-
-    dcc.Graph(id='graph2')],
-    style={'width': '48%', 'display': 'inline-block'})
-
-])
-'''
+#small-cap
 @app.callback(
     dash.dependencies.Output('graph1', 'figure'),
-    [dash.dependencies.Input('sector-dropdown', 'value')])
+    [dash.dependencies.Input('sector1-dropdown', 'value')])
 def update_graph1(country_values):
-    print("type(country_values): ", country_values, type(country_values))
-    dff = df.loc[df['Sector'].isin(country_values)]
+    #print("type(country_values): ", country_values, type(country_values))
+    dff = df1.loc[df1['Sector'].isin(country_values)]
 
     return {
         'data': [go.Scatter(
@@ -136,9 +201,9 @@ def update_graph1(country_values):
             name=Sector
         ) for Sector in dff.Sector.unique()],
         'layout': go.Layout(
-            title="GDP over time, by country",
+            title="S&P small-cap sector-wise",
             xaxis={'title': 'Year'},
-            yaxis={'title': 'GDP Per Capita'},
+            yaxis={'title': 'Cumulative Returns'},
             margin={'l': 60, 'b': 50, 't': 80, 'r': 0},
             hovermode='closest'
         )
@@ -147,10 +212,10 @@ def update_graph1(country_values):
 
 @app.callback(
     dash.dependencies.Output('graph2', 'figure'),
-    [dash.dependencies.Input('symbol-dropdown', 'value')])
+    [dash.dependencies.Input('symbol1-dropdown', 'value')])
 def update_graph2(country_values):
-    print("type(country_values): ", country_values, type(country_values))
-    dff = df_copy.loc[df_copy['Symbol'].isin(country_values)]
+    #print("type(country_values): ", country_values, type(country_values))
+    dff = df1_copy.loc[df1_copy['Symbol'].isin(country_values)]
 
     return {
         'data': [go.Scatter(
@@ -160,13 +225,112 @@ def update_graph2(country_values):
             name=Symbol
         ) for Symbol in dff.Symbol.unique()],
         'layout': go.Layout(
-            title="GDP over time, by country",
+            title="S&P small-cap company-wise",
             xaxis={'title': 'Year'},
-            yaxis={'title': 'GDP Per Capita'},
+            yaxis={'title': 'Cumulative Returns'},
             margin={'l': 60, 'b': 50, 't': 80, 'r': 0},
             hovermode='closest'
         )
     }
+
+
+#mid-cap
+@app.callback(
+    dash.dependencies.Output('graph3', 'figure'),
+    [dash.dependencies.Input('sector2-dropdown', 'value')])
+def update_graph3(country_values):
+    #print("type(country_values): ", country_values, type(country_values))
+    dff = df2.loc[df2['Sector'].isin(country_values)]
+
+    return {
+        'data': [go.Scatter(
+            x=dff[dff['Sector'] == Sector].Date,
+            y=dff[dff['Sector'] == Sector]['daily_cum_returns'],
+            mode='lines',
+            name=Sector
+        ) for Sector in dff.Sector.unique()],
+        'layout': go.Layout(
+            title="S&P mid-cap sector-wise",
+            xaxis={'title': 'Year'},
+            yaxis={'title': 'Cumulative Returns'},
+            margin={'l': 60, 'b': 50, 't': 80, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
+
+@app.callback(
+    dash.dependencies.Output('graph4', 'figure'),
+    [dash.dependencies.Input('symbol2-dropdown', 'value')])
+def update_graph4(country_values):
+    #print("type(country_values): ", country_values, type(country_values))
+    dff = df2_copy.loc[df2_copy['Symbol'].isin(country_values)]
+
+    return {
+        'data': [go.Scatter(
+            x=dff[dff['Symbol'] == Symbol].Date,
+            y=dff[dff['Symbol'] == Symbol]['daily_cum_returns'],
+            mode='lines',
+            name=Symbol
+        ) for Symbol in dff.Symbol.unique()],
+        'layout': go.Layout(
+            title="S&P mid-cap company-wise",
+            xaxis={'title': 'Year'},
+            yaxis={'title': 'Cumulative Returns'},
+            margin={'l': 60, 'b': 50, 't': 80, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
+
+#large-cap
+@app.callback(
+    dash.dependencies.Output('graph5', 'figure'),
+    [dash.dependencies.Input('sector3-dropdown', 'value')])
+def update_graph5(country_values):
+    #print("type(country_values): ", country_values, type(country_values))
+    dff = df3.loc[df3['Sector'].isin(country_values)]
+
+    return {
+        'data': [go.Scatter(
+            x=dff[dff['Sector'] == Sector].Date,
+            y=dff[dff['Sector'] == Sector]['daily_cum_returns'],
+            mode='lines',
+            name=Sector
+        ) for Sector in dff.Sector.unique()],
+        'layout': go.Layout(
+            title="S&P large-cap sector-wise",
+            xaxis={'title': 'Year'},
+            yaxis={'title': 'Cumulative Returns'},
+            margin={'l': 60, 'b': 50, 't': 80, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
+
+@app.callback(
+    dash.dependencies.Output('graph6', 'figure'),
+    [dash.dependencies.Input('symbol3-dropdown', 'value')])
+def update_graph6(country_values):
+    #print("type(country_values): ", country_values, type(country_values))
+    dff = df3_copy.loc[df3_copy['Symbol'].isin(country_values)]
+
+    return {
+        'data': [go.Scatter(
+            x=dff[dff['Symbol'] == Symbol].Date,
+            y=dff[dff['Symbol'] == Symbol]['daily_cum_returns'],
+            mode='lines',
+            name=Symbol
+        ) for Symbol in dff.Symbol.unique()],
+        'layout': go.Layout(
+            title="S&P large-cap company-wise",
+            xaxis={'title': 'Year'},
+            yaxis={'title': 'Cumulative Returns'},
+            margin={'l': 60, 'b': 50, 't': 80, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
